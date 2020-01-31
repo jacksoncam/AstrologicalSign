1b4c9a09a5b059a96f4525799114858f2b6dd406
Cameren Jackson <56235888+jacksoncam@users.noreply.github.com>
2020-01-30 16:23:17 -0800

    Update to Astrological
    Updated month input so now you can enter the month any way you want to ex: January, JANUARY, january

--- AstrologicalSign.py
+++ AstrologicalSign.py
@@ -3,135 +3,232 @@
 #and what element that zodiac sign belongs to
 #At the moment you have to enter the  birth months as followed :
 #"January", "Febuary", "March", "April" , "May","June","July","August","September","October","November","December"
-#Make sure to include the first letter as a capital"""
+"""
 
 import sys
-
-#Signs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo","Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn","Aquarius", "Pisces"]
 inputMonth = input(" Please enter your birth month : ")
 inputDay = int(input("Please enter the day of your birth : "))
-
-#for x in range(1,32):
-
 Months = []
 Days = []
 Days.append(int(inputDay))
 Months.append(inputMonth)
+messageAir = ("Air signs are rational, social, and love communication and relationships")
+messageEarth = ("Earth signs are “grounded” and the ones that bring us down to earth They are mostly conservative and realistic")
+messageFire = ("Fire signs tend to be passionate, dynamic, and temperamental They get angry quickly, but they also forgive easily.")
+messageWater =( "Water signs are exceptionally emotional and ultra-sensitive")
     
-    
-    #print(Days[0],Months[0])
-    
-    
-    
-if inputMonth == "January":
-    
-    
-    Astro = "Capricorn"
-    if (Days[0] >= 20):
-        Astro = "Aquarius"
-    Type = "Air"
-    print(Astro)
-    message = print("Earth signs are “grounded” and the ones that bring us down to earth They are mostly conservative and realistic,")
-    
-      
-elif inputMonth == "Febuary":
-    
-    Astro = "Aquarius"
-    if (Days[0] >=19):
-        Astro = "Pisces"
-    Type = "Water"
-    print(Astro)
-    message = print("Water signs are exceptionally emotional and ultra-sensitive ")
-    
-     
-elif inputMonth == "March":
-    Astro = "Pisces"
-    if (Days[0] >= 21):
-        Astro = "Aries"
-    Type = "Fire"
-    print(Astro)
-    message = print("Fire signs tend to be passionate, dynamic, and temperamental They get angry quickly, but they also forgive easily. ")
-    
-       
-elif inputMonth == "April":
-    Astro = "Aries"
-    if (Days[0] >= 20):
-        Astro = "Taurus"
-    Type = "Earth"
-    print(Astro)
-    message =  print("Earth signs are “grounded” and the ones that bring us down to earth They are mostly conservative and realistic")
-    
-    
-elif inputMonth == "May":
-    Astro = "Taurus"
-    if (Days[0] >= 21):
-        Astro = "Gemini"
-    Type = "Air"
-    print(Astro)
-    message = print("Air signs are rational, social, and love communication and relationships")
-   
-    
-elif inputMonth == "June":
-    Astro = "Gemini"
-    if (Days[0] >= 21):
-        Astro = "Cancer"
-    Type = "Water" 
-    print(Astro)
-    message = print("Water signs are exceptionally emotional and ultra-sensitive ")
-    
-elif inputMonth == "July":
-    Astro = "Cancer"
-    if (Days[0] >= 23):
-        Astro = "Leo"
-    Type = "Fire" 
-    print(Astro)
-    message = print("Fire signs tend to be passionate, dynamic, and temperamental They get angry quickly, but they also forgive easily. ")
 
-elif inputMonth == "August":
-    Astro = "Leo"
-    if (Days[0] >= 23):
-        Astro = "Virgo"
-    Type = "Earth"
-    print(Astro)
-    message =  print("Earth signs are “grounded” and the ones that bring us down to earth They are mostly conservative and realistic")
+jan = ["January","january","JANUARY"]
+feb = ["Febuary","febuary","FEBUARY"]
+mar = ["March","march","MARCH"]
+apr = ["April","april","APRIL"]
+may = ["May","may","MAY"]
+jun = ["June","june","JUNE"]
+jul = ["July","july","JULY"]
+aug = ["August","august","AUGUST"]
+sept = ["September","september","SEPTEMBER"]
+octo = ["October","october","OCTOBER"]
+nov = ["November","november","NOVEMBER"]
+dec = ["December","december","DECEMBER"]
+
+
+if inputMonth == jan[0] or inputMonth == jan[1] or inputMonth == jan[2]:
+    inputMonth = "January"
+if inputMonth == feb[0] or inputMonth == feb[1] or inputMonth == feb[2]:
+    inputMonth = "Febuary"
+if inputMonth == mar[0] or inputMonth == mar[1] or inputMonth == mar[2]:
+    inputMonth = "March"
+if inputMonth == apr[0] or inputMonth == apr[1] or inputMonth == apr[2]:
+    inputMonth = "April"
+if inputMonth == may[0] or inputMonth == may[1] or inputMonth == may[2]:
+    inputMonth = "May"
+if inputMonth == jun[0] or inputMonth == jun[1] or inputMonth == jun[2]:
+    inputMonth = "June"
+if inputMonth == jul[0] or inputMonth == jul[1] or inputMonth == jul[2]:
+    inputMonth = "July"
+if inputMonth == aug[0] or inputMonth == aug[1] or inputMonth == aug[2]:
+    inputMonth = "August"
+if inputMonth == sept[0] or inputMonth == sept[1] or inputMonth == sept[2]:
+    inputMonth = "September"
+if inputMonth == octo[0] or inputMonth == octo[1] or inputMonth == octo[2]:
+    inputMonth = "October"
+if inputMonth == nov[0] or inputMonth == nov[1] or inputMonth == nov[2]:
+    inputMonth = "November"
+if inputMonth == dec[0] or inputMonth == dec[1] or inputMonth == dec[2]:
+    inputMonth = "December"
     
-    
-elif inputMonth == "September":
-    Astro = "Virgo"
-    if (Days[0] >= 23):
-        Astro = "Libra"
-    Type = "Air" 
-    print(Astro)
-    message = print("Air signs are rational, social, and love communication and relationships")
-    
-elif inputMonth == "October":
-    Astro = "Libra"
-    if (Days[0] >= 23):
-        Astro = "Scorpio"
-    Type = "Water" 
-    print(Astro)
-    message = print("Water signs are exceptionally emotional and ultra-sensitive ")
-    
-elif inputMonth == "November":
-    Astro = "Scorpio"
-    if (Days[0] >= 22):
-        Astro = "Sagittarius"
-    Type = "Fire" 
-    print(Astro)
-    message = print("Fire signs tend to be passionate, dynamic, and temperamental They get angry quickly, but they also forgive easily. ")
-    
-elif inputMonth == "December":
-    Astro = "Sagittarius"
-    if (Days[0] >= 22):
+ 
+if inputMonth == "January":
+    if (Days[0] <= 19):
+        print(messageEarth)
         Astro = "Capricorn"
-    Type = "Earth"
-    print(Astro)
-    message =  print("Earth signs are “grounded” and the ones that bring us down to earth They are mostly conservative and realistic")
+        Type = "Earth"
+        
+    elif (Days[0] >= 20):
+        print(messageAir)
+        Astro = "Aquarius"
+        Type = "Air"
+        print(Astro)
+        
+elif inputMonth == "Febuary":
+    if (Days[0] <= 18):
+        print(messageAir)
+        Type = "Air"
+        Astro = "Aquarius"
+        
+    elif (Days[0] >=19):
+        print(messageWater)
+        Astro = "Pisces"
+        Type = "Water"
+        print(Astro)
+        
+elif inputMonth == "March":
+    
+    if (Days[0] <=20):
+        print(messageWater)
+        Astro = "Aries"
+        Type = "Water"
+        print(Astro)
+        
+    elif (Days[0] >= 21):
+        print(messageFire)
+        Astro = "Aries"
+        Type = "Fire"
+        print(Astro)
+        
+elif inputMonth == "April":
+    
+    if (Days[0] <= 19):
+        print(messageFire)
+        Astro = "Aries"
+        Type = "Fire"
+        print(Astro)
+        
+    elif (Days[0] >= 20):
+        print(messageEarth)
+        Astro = "Taurus"
+        Type = "Earth"
+        print(Astro)
+        
+elif inputMonth == "May":
+    
+    if (Days[0] <= 20):
+        print(messageEarth)
+        Astro = "Taurus"
+        Type = "Earth"
+        print(Astro)
+        
+    elif (Days[0] >= 21):
+        print(messageAir)
+        Astro = "Gemini"
+        Type = "Air"
+        print(Astro)
+         
+elif inputMonth == "June":
+    
+    if (Days[0] <= 20):
+        print(messageAir)
+        Astro = "Gemini"
+        Type = "Air"
+        print(Astro)
+        
+    elif (Days[0] >= 21):
+        print(messageWater)
+        Astro = "Cancer"
+        Type = "Water" 
+        print(Astro)
+        
+elif inputMonth == "July":
+    
+    if (Days[0] <= 22):
+        print(messageWater)
+        Astro = "Cancer"
+        Type = "Water"
+        print(Astro)
+        
+    elif (Days[0] >= 23):
+        print(messageFire)
+        Astro = "Leo"
+        Type = "Fire" 
+        print(Astro)
+        
+elif inputMonth == "August":
+    
+    if (Days[0] <= 22):
+        print(messageFire)
+        Astro = "Leo"
+        Type = "Fire"
+        print(Astro)
+        
+    elif (Days[0] >= 23):
+        print(messageEarth)
+        Astro = "Virgo"
+        Type = "Earth"
+        print(Astro)
+        
+elif inputMonth == "September":
+    
+    if (Days[0] <= 22):
+        print(messageEarth)
+        Astro = "Virgo"
+        Type = "Earth"
+        print(Astro)
+          
+    elif (Days[0] >= 23):
+        print(messageAir)
+        Astro = "Libra"
+        Type = "Air" 
+        print(Astro)
+        
+elif inputMonth == "October":
+    
+    if (Days[0] <= 22):
+        print(messageAir)
+        Astro = "Libra"
+        Type = "Air"
+        print(Astro)
+        
+    elif (Days[0] >= 23):
+        print(messageWater)
+        Astro = "Scorpio"
+        Type = "Water" 
+        print(Astro)
+        
+elif inputMonth == "November":
+    
+    if (Days[0] <= 21):
+        print(messageWater)
+        Astro = "Scorpio"
+        Type = "Water"
+        print(Astro)
+    
+    elif (Days[0] >= 22):
+        print(messageFire)
+        Astro = "Sagittarius"
+        Type = "Fire" 
+        print(Astro)
+        
+elif inputMonth == "December":
+    
+    if (Days[0] <= 21):
+        print(messageFire)
+        Astro = "Sagittarius"
+        Type = "Fire"
+        print(Astro)
+        
+    if (Days[0] >= 22):
+        print(messageEarth)
+        Astro = "Capricorn"
+        Type = "Earth"
+        print(Astro)
         
 else:
+    
         print("Not a Valid Month")
         sys.exit()
         
+
+
 print("Your birthday is : ", inputMonth , inputDay, ", And your Zodiac sign is : ", Astro , " , The element of the sign is : ", Type)
 
 
